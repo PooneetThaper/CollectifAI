a9056fe381932744ab1fcd0b98a145fa8f8ca3e7
Jane Chen <jane.chen@macaulay.cuny.edu>
2016-10-09 09:53:43 -0400

    updated search

--- front/search.php
+++ front/search.php
@@ -2,4 +2,13 @@
 $tag = $_POST["tag"];
 // pass arguments as command line arguments
 exec("../api.py f $tag");
+
+// this piece of code is from w3schools
+$myfile = fopen("out.txt", "r") or die("Unable to open file!");
+// Output one line until end-of-file
+while(!feof($myfile)) {
+  echo fgets($myfile) . "<br>";
+}
+fclose($myfile);
+
 ?>
