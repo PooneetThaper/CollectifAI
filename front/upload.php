a92441206f30aa86e31fae2586e9bc29da9fd971
Jane Chen <jane.chen@macaulay.cuny.edu>
2016-10-09 09:44:07 -0400

    updated legal file types

--- front/upload.php
+++ front/upload.php
@@ -8,42 +8,43 @@ $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
+	$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+	if($check !== false) {
+		echo "File is an image - " . $check["mime"] . ".";
+		$uploadOk = 1;
+	} else {
+		echo "File is not an image.";
+		$uploadOk = 0;
+	}
 }
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
+	echo "Sorry, file already exists.";
+	$uploadOk = 0;
 }
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
+	echo "Sorry, your file is too large.";
+	$uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "jpeg") {
-    echo "Sorry, only JPG and JPEG files are allowed.";
-    $uploadOk = 0;
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+	echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+	$uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
+	echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+		echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 
-        // pass arguments as command line arguments
+		// pass arguments as command line arguments
 		exec("../api.py a $target_file");
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
+	} else {
+		echo "Sorry, there was an error uploading your file.";
+	}
 }
 ?>
