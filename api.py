9869831c0f8f61e78f9c00b1157a3611c84ff391
Pooneet Thaper <PT@172-30-30-190.DYNAPOOL.NYU.EDU>
2016-10-09 08:14:17 -0400

    accepting input from front for database input and queries

--- api.py
+++ api.py
@@ -1,60 +1,44 @@
+import sys
 from pymongo import MongoClient
 from clarifai.client import ClarifaiApi
-import sys
 
-#ClarifaiApi stuff
-
-clarifai_api = ClarifaiApi("5ufSEyCVOzwUzNX5nDWPFXaTHbOTkbS3NzAGyrNa","JbqnMT_wofMV1IS-QZ0jIsYo-60tdHkQOFfqGxM7")
-
-urls = ['https://digitalmarketing.blob.core.windows.net/6305/images/thumbs/image124265.jpg',
-    'http://media.vanityfair.com/photos/56cb5d18ab73e22d6d9321f6/master/h_590,c_limit/donald-trump-short-fingered-vulgarian-fingers-bruce-handy-ss13.jpg']
-
-tagHolder=[]
-
-i=0
-
-while i<len(urls):
-    result = clarifai_api.tag_image_urls(urls[i])
-    imageTags = []
-    for tag in result['results'][0]['result']['tag']['classes']:
-        tag = tag.encode('ascii', 'ignore')
-        imageTags.append(tag)
-    tagHolder.append(imageTags)
-    i+=1
-
-j=0
-while j<i:
-    #print urls[j]
-    #for imageTags in tagHolder[j]:
-    #    print imageTags
-    j+=1
-
-#MongoDB stuff
-
-##Adding stuff
+opType=sys.argv[1]
+argtext=sys.argv[2]
 
 client = MongoClient()
 db = client.database
-"""
-j=0
-while j<i:
+
+def add():
+    tagHolder=[]
+    i=0
+    #ClarifaiApi stuff
+    clarifai_api = ClarifaiApi("5ufSEyCVOzwUzNX5nDWPFXaTHbOTkbS3NzAGyrNa","JbqnMT_wofMV1IS-QZ0jIsYo-60tdHkQOFfqGxM7")
+    path='front/uploads/'+argtext
+    result = clarifai_api.tag_images(open(path, 'rb'))
+    for tag in result['results'][0]['result']['tag']['classes']:
+        tag = tag.encode('ascii', 'ignore')
+        tagHolder.append(tag)
+    #MongoDB stuff
+    ##Adding stuff
     result = db.photos.insert_one(
         {
-            "url":urls[j],
-            "tags":tagHolder[j]
+            "path":path,
+            "tags":tagHolder
         }
     )
     print result.inserted_id
-    j+=1
 
-"""
-##Finding stuff
+def find():
+    paths=''
+    cursor = db.photos.find({"tags":argtext})
+    for document in cursor:
+        paths.append(document.path)
+        paths.append('\n')
+    f = open('front/out.txt', 'w')
+    f.write(paths)
 
-cursor = db.photos.find({"tags":"potato"})
-for document in cursor:
-    print(document)
-
-
-###### trying to get a or f parameters
-print sys.argv[1] # this is the a or f argument
-print sys.argv[2] # this is the url or tag
+if __name__ == "__main__":
+    if(opType=='a'):
+        add()
+    else:
+        find()
